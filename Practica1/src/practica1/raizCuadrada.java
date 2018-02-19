3f8e9569d8e2c51744338a4561dbc0b33c0191b7
D201325560 <d.daniel19@hotmail.com>
2018-02-18 21:56:15 -0600

    Función raiz Cuadrada
    Función a la cual se le manda de parametro un número entero, se le saca raiz cuadrada y devuelve un valor double.

---
+++ Practica1/src/practica1/raizCuadrada.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author SAMSUNG
+ */
+public class raizCuadrada {
+    public double raizCuadrada(int val){
+        return Math.sqrt(val);
+    }
+}
